48f34b6d5bf3e4ef7a1a55ecf34e9e591a1e27f1
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-08-01 16:12:46 -0400

    added edit and delete functionality

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ fetch(`${baseURL}/todos`)
 
 
 function generateTodoTemplate({ id, title, completed }) {
-  const editSVG = `<svg class="edit__btn" name="edit__btn--${id}" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>`;
-  const deleteSVG = `<svg class="delete__btn" name="delete__btn--${id}" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>`;
+  const editSVG = `<svg class="edit__btn" name="${id}" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>`;
+  const deleteSVG = `<svg class="delete__btn" name="${id}" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>`;
 
   return `<section id=${id}>
   <p class='todo__item ${completed ? "--completed" : ""}' name='${id}'>${title}</p>
@@ -65,18 +65,69 @@ function onSubmit(event) {
 }
 
 function completeTodo(event) {
+  event.preventDefault();
+  const completedTag = event.target.className.split(" ")[1];
+  const isCompleted = completedTag ? true : false;
   const id = +event.target.getAttribute("name");
   
-  fetch(`${baseURL}/todos/${id}`, {
+  if (isCompleted) {
+    fetch(`${baseURL}/todos/${id}`, {
+    method: "PATCH",
+    headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json;charset=UTF-8'
+    },
+    body: JSON.stringify({completed: false})
+    })
+  } else {
+    fetch(`${baseURL}/todos/${id}`, {
     method: "PATCH",
     headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json;charset=UTF-8'
     },
     body: JSON.stringify({completed: true})
-  })
+    })
+
+  }
+  
 }
 
+function editTodo(event) {
+  event.preventDefault();
+  const id = event.target.getAttribute("name")
+  const todoContainer = document.getElementById(id);
+  const form = document.createElement("form");
+  const input = `<input class="input--edit" type="text" name="todo" />`;
+  const todo = todoContainer.childNodes[1];
+  form.addEventListener("submit", submitEdit);
+  form.innerHTML = input
+
+  todoContainer.insertBefore(form, todo);
+  todo.remove();
+
+  function submitEdit() {
+    const newTodo = document.querySelector(".input--edit").value;
+
+    fetch(`${baseURL}/todos/${id}`, {
+      method: "PUT",
+    headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json;charset=UTF-8'
+    },
+    body: JSON.stringify({title: newTodo, completed: false})
+    })
+
+  }
+}
+
+
+function deleteTodo(event) {
+  event.preventDefault();
+  const id = event.target.getAttribute("name");
+  console.log(id)
+  fetch(`${baseURL}/todos/${id}`, {method: "DELETE"})
+}
 
 formElement.addEventListener("submit", onSubmit)
 
