6689ab52c53439f04cbfaa300de1e3c1e68a9aa1
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-08-01 14:08:37 -0400

    added fetched data to local state

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-function main() {
-  
+// STATE
+class State {
+  constructor() {
+    this._todos = [];
+  }
+
+  get todos() {
+    return this._todos; 
+  }
+
+  set todos(newTodo) {
+    this._todos = newTodo;
+    renderTodos();
+  }
 }
 
-main();
+// CONST
+const state = new State();
+const todoContainer = document.querySelector(".todo__container");
+const formElement = document.querySelector(".form__container");
+const defaultMsg = "<p>no active task</p>"
+
+fetch("http://localhost:3000/todos")
+  .then((res) => res.json())
+  .then((json) => state.todos = json)
+
+
+
+
+formElement.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputElement = document.querySelector(".input");
+  console.log(state)
+})
+
