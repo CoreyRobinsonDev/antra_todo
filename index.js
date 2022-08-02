b138c46eeb3f804e324c62e801f40f1b248a6d4a
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-08-01 23:10:55 -0400

    made events triggered by svg elements more reliably triggered

--- index.js
+++ index.js
@@ -14,10 +14,9 @@ class State {
     const todoItems = document.querySelectorAll(".todo__item");
     const editBtns = document.querySelectorAll(".edit__btn");
     const deleteBtns = document.querySelectorAll(".delete__btn");
-    editBtns.forEach(btn => btn.addEventListener("click", editTodo))
-    deleteBtns.forEach(btn => btn.addEventListener("click", deleteTodo))
-    todoItems.forEach(todo => todo.addEventListener("click", completeTodo))
-    console.log(this)
+    editBtns.forEach(btn => btn.addEventListener("click", editTodo));
+    deleteBtns.forEach(btn => btn.addEventListener("click", deleteTodo));
+    todoItems.forEach(todo => todo.addEventListener("click", completeTodo));
   }
 }
 
@@ -34,8 +33,8 @@ fetch(`${baseURL}/todos`)
 
 
 function generateTodoTemplate({ id, title, completed }) {
-  const editSVG = `<svg class="edit__btn" name="${id}" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>`;
-  const deleteSVG = `<svg class="delete__btn" name="${id}" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>`;
+  const editSVG = `<button class="edit__btn"><svg name="${id}" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path name="${id}" d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg></button>`;
+  const deleteSVG = `<button class="delete__btn"><svg name="${id}" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path name="${id}" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg></button>`;
 
   return `<section id=${id} class="todo__item--section">
   <p class='todo__item ${completed ? "--completed" : ""}' name='${id}'>${title}</p>
@@ -91,14 +90,12 @@ function completeTodo(event) {
     },
     body: JSON.stringify({completed: true})
     })
-
   }
-  
 }
 
 function editTodo(event) {
   event.preventDefault();
-  const id = event.target.getAttribute("name")
+  const id = event.target.getAttribute("name");
   const todoContainer = document.getElementById(id);
   const form = document.createElement("form");
   const todo = todoContainer.childNodes[1];
